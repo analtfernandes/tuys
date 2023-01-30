104b79161f1e72a8288216c5858c880592518436
Ana Fernandes <analuizateixeirafernandes184@gmail.com>
2023-01-30 16:17:38 -0300

    fix: redirect to correct page on refresh

--- src/components/privatePage/PrivatePage.tsx
+++ src/components/privatePage/PrivatePage.tsx
@@ -1,17 +1,23 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useUserContext } from "../../contexts/UserContext";
 
 export function PrivatePage({ children }: React.PropsWithChildren) {
 	const { user } = useUserContext();
+	const location = useLocation();
 	const navigate = useNavigate();
+	const locationRef = useRef(location);
+
+	useEffect(() => {
+		if (location.pathname !== "/sign-in") locationRef.current = location;
+	}, [location]);
 
 	useEffect(() => {
 		if (!user.token) {
 			navigate("/sign-in");
 		}
 		if (user.token) {
-			navigate("/");
+			navigate(locationRef.current.pathname, { ...locationRef.current });
 		}
 	}, [user]);
 
