842b4beee15e4fc3ce78adeca5ccc14f45d11142
analtfernandes <analuizateixeirafernandes184@gmail.com>
2023-07-15 16:26:08 -0300

    refactor: add clear local storage in navigate to sign-in hook

--- frontend/src/hooks/navigate-sign-in/useNavigateSignIn.ts
+++ frontend/src/hooks/navigate-sign-in/useNavigateSignIn.ts
@@ -1,15 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import { useToast } from "../index";
+import { useToast, useLocalStorage } from "../index";
 
 function useNavigateSignIn() {
 	const navigate = useNavigate();
 	const toast = useToast();
+	const { clearLocalStorage } = useLocalStorage();
 
 	return () => {
 		toast({
 			type: "warning",
 			text: "Sessão encerrada.",
 		});
+		clearLocalStorage();
 		navigate("/sign-in");
 	};
 }
