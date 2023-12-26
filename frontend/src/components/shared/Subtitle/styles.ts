0cc2594733ce55b8638d254cdab9ab7d947a4b25
anatfernandes <analuizateixeirafernandes184@gmail.com>
2023-12-26 09:55:04 -0300

    refactor: refactor subtitle shared component

--- frontend/src/components/shared/Subtitle/styles.ts
+++ frontend/src/components/shared/Subtitle/styles.ts
@@ -1,12 +1,10 @@
 import styled from "styled-components";
 
-export function Subtitle({ children }: React.PropsWithChildren) {
-	return <Wrapper>{children}</Wrapper>;
-}
-
 const Wrapper = styled.span`
 	font-size: 1.1rem;
 	line-height: 20px;
 	font-weight: 400;
 	color: ${(props) => props.theme.colors.text};
 `;
+
+export { Wrapper };
